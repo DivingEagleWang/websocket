8030f2baf4d69b1a9b283c51fc4e6b73e487611a
wangli <wl1337076504@163.com>
2019-10-28 14:17:50 +0800

    first commit

---
+++ src/main/java/com/test/websockettest/config/WebSocketConfig.java
@@ -0,0 +1,17 @@
+package com.test.websockettest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * Created by wangli
+ * 2019/10/28  13:33
+ */
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter(){
+        return new ServerEndpointExporter();
+    }
+}
---
+++ src/main/java/com/test/websockettest/controller/WebSocketController.java
@@ -0,0 +1,40 @@
+package com.test.websockettest.controller;
+
+import com.test.websockettest.server.WebSocketServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by wangli
+ * 2019/10/28  13:38
+ */
+@RestController
+@RequestMapping("/api/ws")
+public class WebSocketController {
+    @GetMapping(value = "/sendAll")
+    public String sendAllMessage(@RequestParam(required = true) String message){
+        try {
+            WebSocketServer.BroadCastInfo(message);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return "ok";
+    }
+    /**
+     * 指定会话ID发消息
+     * @param message 消息内容
+     * @param id 连接会话ID
+     * @return
+     */
+    @GetMapping(value="/sendOne")
+    public String sendOneMessage(@RequestParam(required=true) String message,@RequestParam(required=true) String id){
+        try {
+            WebSocketServer.SendMessage(message,id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "ok";
+    }
+}
